644d57adb93a94ccf10fbb3d38bf4ed006ee4e6b
Muhammet Esen <muhammet.esen@grassfish.com>
2023-05-08 14:49:49 +0200

    remove unused import

--- src/app/add-drive/add-drive.component.ts
+++ src/app/add-drive/add-drive.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-drive',
@@ -16,7 +15,7 @@ export class AddDriveComponent {
     { label: 'Bar', value: 'bar' }
   ];
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   setNewDate() {
     this.time = new Date().toLocaleTimeString();
