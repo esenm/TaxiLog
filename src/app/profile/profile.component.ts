6ddf8d41e88770de67eac1fbf96f3896260c7098
Muhammet Esen <muhammet.esen@grassfish.com>
2023-05-09 14:50:53 +0200

    update profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,5 +10,16 @@ export class ProfileComponent {
   name: string = 'Vincent Aboubakar';
   phone: string = '+43 650 421 8662';
   code: string = 'W-1903 TX';
+  avatarText: string = '';
 
+  updateProfileInfo() {
+    // Hier können Sie den Code schreiben, um die aktualisierten Profilinfos zu speichern
+    console.log('Aktualisierte Profilinfos:', this.name, this.phone, this.code);
+  }
+
+  ngOnInit() {
+    this.avatarText = this.name.charAt(0).toUpperCase();
+
+    this.updateProfileInfo();
+  }
 }
