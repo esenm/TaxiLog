2ae60710cf1c89326536b6902fb7e3c3bdf737dd
Muhammet Esen <muhammet.esen@grassfish.com>
2023-05-09 15:43:23 +0200

    update profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -12,14 +14,18 @@ export class ProfileComponent {
   code: string = 'W-1903 TX';
   avatarText: string = '';
 
+  constructor(private router: Router) {}
+
   updateProfileInfo() {
     // Hier können Sie den Code schreiben, um die aktualisierten Profilinfos zu speichern
     console.log('Aktualisierte Profilinfos:', this.name, this.phone, this.code);
   }
 
+  logout() {
+    this.router.navigate(['']);
+  }
+
   ngOnInit() {
     this.avatarText = this.name.charAt(0).toUpperCase();
-
-    this.updateProfileInfo();
   }
 }
