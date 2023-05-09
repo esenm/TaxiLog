9024bce0c0e24c55156f836ce896938c35c43068
Muhammet Esen <muhammet.esen@grassfish.com>
2023-05-09 14:30:49 +0200

    update profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  name: string = 'Vincent Aboubakar';
+  phone: string = '+43 650 421 8662';
+  code: string = 'W-1903 TX';
+
 }
